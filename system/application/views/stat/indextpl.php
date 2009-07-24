730bc039e01632a3de9cf27246a052b9bd6b59e7
aquilax <aquilax@cbac6fcc-66b9-11de-8f04-0fb5c4d7e880>
2009-07-24 06:19:48 +0000

    Statistics layout fixed

--- system/application/views/stat/indextpl.php
+++ system/application/views/stat/indextpl.php
@@ -1,22 +1,30 @@
-<table>
+<table id="stat">
   <tr>
-    <th><?php echo lang('Total Characters')?></th>
+    <td><?php echo lang('Total Characters')?></td>
     <td><?php echo $stat['total_characters']?></td>
   </tr>
   <tr>
-    <th><?php echo lang('Online Characters')?></th>
+    <td><?php echo lang('Online Characters')?></td>
     <td><?php echo $stat['online_characters']?></td>
+  </tr>
+  <tr>
+    <th colspan="2"><?php echo lang('Characters by race')?></th>
   </tr>
 <?php
   foreach($stat['race'] as $name => $count){
     echo '  <tr>';
-    echo '    <th>'.$name.'</th>';
+    echo '    <td>'.$name.'</td>';
     echo '    <td>'.$count.'</td>';
     echo '  </tr>';
   }
+?>
+  <tr>
+    <th colspan="2"><?php echo lang('Characters by map')?></th>
+  </tr>
+<?php
   foreach($stat['map'] as $name => $count){
     echo '  <tr>';
-    echo '    <th>'.$name.'</th>';
+    echo '    <td>'.$name.'</td>';
     echo '    <td>'.$count.'</td>';
     echo '  </tr>';
   }
